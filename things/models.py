5ca3970c474062d17c81aa3ba4e10c558e5228d5
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-17 12:45:21 +0100

    Add model with limitations

--- things/models.py
+++ things/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models import Model
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Object(Model):
+    name = models.TextField(
+        unique = True,
+        blank = False,
+        max_length = 30,
+    )
+    description = models.TextField(
+        unique = False,
+        blank = True,
+        max_length = 120
+    )
+    quantiy = models.PositiveIntegerField(
+        unique = False,
+        validators = [
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ],
+    )
+
